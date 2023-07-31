54dfe1936308f3cefa85431512a4b3a20a518b96
Mogus <example@example.com>
2023-07-31 21:16:33 +0300

    adding keepassxc

---
+++ secrets/secrets.nix
@@ -0,0 +1,9 @@
+let
+  mur = "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIGqfxDRtHtcmQUkLKA1x18nOLv7YHarcQXRpBX/YDDiA screwedmobile";
+in {
+  "finsocks.age".publicKeys = [ mur ];
+  "frsocks.age".publicKeys = [ mur ];
+  "cansocks.age".publicKeys = [ mur ];
+  "mur_password.age".publicKeys = [ mur ];
+  "asr.age".publicKeys = [ mur ]; 
+}
